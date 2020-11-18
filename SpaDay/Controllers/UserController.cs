8f59d7bbf22a85d205555f45f49293e8c8e2d9cc
atparris <atparris93@gmail.com>
2020-11-17 19:05:51 -0600

    changes were made

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route ("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            if (newUser.Password == verify)
+            {
+                ViewBag.user = newUser;
+                ViewBag.username = newUser.Username;
+                ViewBag.email = newUser.Email;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do not match.";
+                return View("Add");
+            }
+        }
+    }
+}
